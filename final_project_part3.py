cd9bdddfaa3326f1b8c34775b5204b4f6cd8edef
Aidan Froggatt <aidanfr@live.ca>
2023-11-24 22:53:29 -0500

    added todos for part 3

--- final_project_part3.py
+++ final_project_part3.py
@@ -1,42 +1,6 @@
-from final_project_part1 import DirectedWeightedGraph
+#TODO: Convert connections and stations csv files into a graph
 
+#TODO: Create a heuristic function for A* algorithm
 
-def print_all_csv_data():
-    london_connections = open("Data/london_connections.csv", "r")
+#TODO: Run A* and Dijkstra's algorithm on the graph, comparing the runtimes
 
-    for row in london_connections:
-        print(row)
-
-    london_connections.close()
-
-    london_stations = open("Data/london_stations.csv", "r")
-
-    for row in london_stations:
-        print(row)
-
-    london_stations.close()
-
-
-def make_underground_graph():
-    london_connections = open("Data/london_connections.csv", "r")
-    london_stations = open("Data/london_stations.csv", "r")
-
-    G = DirectedWeightedGraph()
-
-    for row in london_stations:
-        if row[0] != "id" and row[0] != "\"":
-            G.add_node(row[0])
-
-    for row in london_connections:
-        if row[0] != "\"" and row[0] != "," and row[1] != ",":
-            weight = 1
-            G.add_edge(row[0], row[1], weight)
-
-    london_connections.close()
-    london_stations.close()
-
-    return G
-
-
-G = make_underground_graph()
-print(G.adj)
