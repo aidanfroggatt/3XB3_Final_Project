e33ba2fb24dbad64328b2d264a35c8f219e25fe2
Aidan Froggatt <aidanfr@live.ca>
2023-11-30 11:06:18 -0500

    updated broken station problem

--- final_project_part4.py
+++ final_project_part4.py
@@ -1,53 +1,57 @@
 def bsp_value(L, m):
+    # Sort the list of stations in ascending order
     n = len(L)
-    # The following initializes the dynamic programming table to store the maximum possible minimum distance
-    dynamic_programmer = [[float('inf')] * (m + 1) for _ in range(n + 1)] 
-    L = [0] + L  # To make indexing match the station numbers
+    L.sort()
 
-    # This initializes the base case for the dynamic programming table
-    for i in range(n + 1):
-        dynamic_programmer[i][0] = float('inf')
+    # Define a helper function to check if a given distance is feasible
+    def check(distance):
+        count = 0
+        last_station = float('-inf')
 
-    # This finds the maximum minimum distance
-    for i in range(1, n + 1):
-        for j in range(1, min(m, i) + 1):
-            for k in range(i):
-                dynamic_programmer[i][j] = min(dynamic_programmer[i][j], max(dynamic_programmer[k][j - 1], L[i] - L[k + 1]))
+        # Iterate through the stations to count the number of removed stations
+        for station in L:
+            if station - last_station >= distance:
+                last_station = station
+            else:
+                count += 1
 
-    return dynamic_programmer[n][m]
+        # Check if the count of removed stations is within the allowed limit
+        return count <= m
+
+    # Binary search for the maximum possible distance between adjacent stations
+    low, high = 0, L[-1] - L[0]
+
+    while low < high:
+        mid = (low + high + 1) // 2
+        if check(mid):
+            low = mid
+        else:
+            high = mid - 1
+
+    # Return the maximum possible distance between adjacent stations
+    return low
+
 
 def bsp_solution(L, m):
+    # Sort the list of stations in ascending order
     n = len(L)
-    # Initializing a dynamic programming table to store the maximum possible minimum distance
-    dynamic_programmer = [[float('inf')] * (m + 1) for _ in range(n + 1)]
-    L = [0] + L  # To make indexing match the station numbers
-    chosen = [[-1] * (m + 1) for _ in range(n + 1)]
+    L.sort()
 
-    # Initializing a base case for the dynamic programming table
-    for i in range(n + 1):
-        dynamic_programmer[i][0] = float('inf')
-
-    # This presents a dynamic programming approach for computing maximum possible minimum distance and keeping track of which station is chosen
-    for i in range(1, n + 1):
-        for j in range(1, min(m, i) + 1):
-            for k in range(i):
-                distance = max(dynamic_programmer[k][j - 1], L[i] - L[k + 1])
-                if distance < dynamic_programmer[i][j]:
-                    dynamic_programmer[i][j] = distance
-                    chosen[i][j] = k
-
-    # The following rebuilds the solution from the chosen stations
     result = []
-    idx = n
-    for i in range(m, 0, -1):
-        result.append(L[chosen[idx][i] + 1])
-        idx = chosen[idx][i]
+    last_station = float('-inf')
 
-    result.sort()
+    # Iterate through the stations to construct the solution based on the computed distance
+    for station in L:
+        if station - last_station >= bsp_value(L, m):
+            result.append(station)
+            last_station = station
+
+    # Return the final list of stations satisfying the distance condition
     return result
 
-# Test cases
+
+# Example usage:
 L = [2, 4, 6, 7, 10, 14]
 m = 2
-print("bsp_value([2, 4, 6, 7, 10, 14], 2) =", bsp_value(L, m))
-print("bsp_solution([2, 4, 6, 7, 10, 14], 2) =", bsp_solution(L, m))
+print(bsp_value(L, m))  # Output: 4
+print(bsp_solution(L, m))  # Output: [2, 6, 10, 14]
